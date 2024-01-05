1ee17591cc78bf306ef7b7c99374e4caa321b961
shashi-123jha <ranjanshashi854105@gmail.com>
2024-01-05 19:54:36 +0530

    newforthesame

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const fs =require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const index = fs.readFileSync('index.html')
+const index = fs.readFileSync('./index.html')
 const Home = fs.readFileSync("./index.html")
 const about = fs.readFileSync("./about.html")
 const contact = fs.readFileSync("./contact.html")
